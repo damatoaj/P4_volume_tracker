47ceb6ba9bc4c10fd76ae1e48851e63e03b8ef29
damatoaj <mr.arthurdamato@gmail.com>
2021-03-15 12:57:47 -0700

    added models

--- volume_tracker/models/users_workouts.js
+++ volume_tracker/models/users_workouts.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class user extends Model {
+  class users_workouts extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,17 +11,14 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.user.hasMany(models.workout)
     }
   };
-  user.init({
-    fname: DataTypes.STRING,
-    lname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    pasword: DataTypes.STRING
+  users_workouts.init({
+    userId: DataTypes.INTEGER,
+    workoutId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'user',
+    modelName: 'users_workouts',
   });
-  return user;
+  return users_workouts;
 };
--- volume_tracker/models/workout.js
+++ volume_tracker/models/workout.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.workout.belongsTo(models.user)
     }
   };
   workout.init({
