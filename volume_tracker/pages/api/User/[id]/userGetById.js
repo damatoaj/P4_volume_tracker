a6478482ec1ad4f444878495397823106f2d2f2e
damatoaj <mr.arthurdamato@gmail.com>
2021-03-15 15:33:20 -0700

    finished the last crud route

--- volume_tracker/pages/api/User/[id]/userGetById.js
+++ volume_tracker/pages/api/User/[id]/userGetById.js
@@ -2,7 +2,12 @@ const db = require('../../../../db/models')
 
 
 export default async function getUserById(req, res) {
-    const userGet = await db.user.findOne(req.body.email);
+    const userGet = await db.user.findOne({
+        where: {email: req.body.email}
+    });
     // res.json({byId: req.query.id, message: 'get user by id'})
+    console.log(req.query)
+    console.log(req.query.id)
+    console.log(req.body)
     res.json(userGet);
 }
