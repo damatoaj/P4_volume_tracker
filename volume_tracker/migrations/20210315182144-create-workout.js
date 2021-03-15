de5c73deea311975f216be33751f30b7cdd11421
damatoaj <mr.arthurdamato@gmail.com>
2021-03-15 11:28:05 -0700

    migrated databases

---
+++ volume_tracker/migrations/20210315182144-create-workout.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('workouts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.INTEGER
+      },
+      minutes: {
+        type: Sequelize.INTEGER
+      },
+      heartRate: {
+        type: Sequelize.INTEGER
+      },
+      volume: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('workouts');
+  }
+};
